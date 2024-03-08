32757042cfca1b7213cf614114512c014bb08420
Pedro Santos <psgbi123@gmail.com>
2024-03-08 09:30:42 -0300

    Fibonacci concluido

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,15 @@
 def fibonacci(num):
     fib1 = 0
     fib2 = 1
-    while num < fib1 + fib2:
+    while num > fib1 + fib2:
         fib1, fib2 = fib2, fib1+fib2
-    if num = fib1+fib2 :
+    if num == fib1+fib2:
         return True
     else:
         return False
+
+numero = int(input("digite um numero"))
+if fibonacci(numero) == True:
+    print(str(numero) + " pertence a sequencia")
+else:
+    print(str(numero) + " nao pertence a sequencia")
